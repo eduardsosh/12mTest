de8be860683250bcac674ea0cd7009fcf18c53bb
eduardsosh01~ <eduardsosh@gmail.com>
2022-02-15 13:30:13 +0200

    3rd

--- src/App.java
+++ src/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, git!");
         System.out.println("Hello, also world!");
+
+        int x = 0;
+        x= 1+1;
     }
 }
